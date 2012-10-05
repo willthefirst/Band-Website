943c92ca1513edd98e4356eb2056202d83203fe1
willthefirst <willthefirst@gmail.com>
2012-10-04 23:18:43 -0600

    working sticky navigation link

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,7 +1,16 @@
 var BLOG = {
 
-	stickTheNav: function() { 
-		var stickyTop = ($('.js_sticky').offset().top + 120);
+	stickTheNav: function() {
+		var stickyTop, stickyText;
+
+		var setTops = function() {
+			console.log('resetTop');
+			stickyTop = ($('.js_sticky').offset().top + 120);
+			stickyText = stickyTop - 45;
+		};
+
+		setTops();
+		console.log(stickyTop);
 
 		$(window).scroll(function() {
 			var windowTop = $(window).scrollTop();
@@ -9,26 +18,38 @@ var BLOG = {
 			if (stickyTop < windowTop) {
 				$('.js_sticky').css({
 					position: 'fixed',
-					top: '-120px'
-				});
-
-				$('.anchorLink').css({
-					top: '155px'
+					top: '180px'
 				});
 			}
+
 			else {
 				$('.js_sticky').css({
-					position: 'static'
+					position: 'absolute',
+					top: '100%'
 				});
+			}
 
+			if (stickyText < windowTop) {
 				$('.anchorLink').css({
-					top: '110px'
+					position: 'fixed',
+					top: '35px',
+					left: '50%',
+					'margin-left': '-55px'
+				});
+			}
+
+			else {
+				$('.anchorLink').css({
+					position: '',
+					top: '',
+					left: '',
+					'margin-left': ''
 				});
 			}
 		});
 
 		$(window).resize(function() {
-			stickyTop = ($('.js_sticky').offset().top + 120);
+			setTops();
 		});
 	}
 
